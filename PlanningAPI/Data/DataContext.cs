c527ca7d4251ff408f378e164295597366f7fc5a
Anna <anna.zhuk.dreamer@yandex.ru>
2024-06-28 02:25:39 +0300

    создан проект, добавлена бд

---
+++ PlanningAPI/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlanningAPI.Models;
+
+namespace PlanningAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<ToDoTask> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=planningdb;Username=postgres;Password=");
+        }
+    }
+}
---
+++ PlanningAPI/Models/ToDoTask.cs
@@ -0,0 +1,11 @@
+﻿namespace PlanningAPI.Models
+{
+    public class ToDoTask
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public required bool Status { get; set; }
+        public uint? Time { get; set; }
+    }
+}
